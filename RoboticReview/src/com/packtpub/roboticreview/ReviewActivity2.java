b560bf539a90c4987af9a5f492b346e12884a67a
Brian Kang <kang36897@gmail.com>
2012-05-01 14:05:38 +0800

    finish chapter 3

--- RoboticReview/src/com/packtpub/roboticreview/ReviewActivity2.java
+++ RoboticReview/src/com/packtpub/roboticreview/ReviewActivity2.java
@@ -33,10 +33,11 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TabHost.OnTabChangeListener;
 import android.widget.ViewSwitcher.ViewFactory;
 
-public class ReviewActivity extends TabActivity implements ViewFactory,
+public class ReviewActivity2 extends TabActivity implements ViewFactory,
 		Runnable, OnItemSelectedListener, OnSeekBarChangeListener,
-		OnClickListener, OnDateSetListener, OnTimeSetListener ,OnTabChangeListener{
-
+		OnClickListener, OnDateSetListener, OnTimeSetListener,
+		OnTabChangeListener {
+	private TabHost tabHost;
 	private final Handler switcherCommentsHandler = new Handler();
 	private TextSwitcher switcher;
 	private String[] comments;
@@ -53,14 +54,18 @@ public class ReviewActivity extends TabActivity implements ViewFactory,
 	private SimpleDateFormat timeFormat;
 	private Button time;
 
+	private boolean reviewInit = false;
+	private boolean photoInit = false;
+	private boolean reservationInit = false;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		TabHost tabHost = getTabHost();
-		getLayoutInflater().inflate(R.layout.main, tabHost.getTabContentView(),
-				true);
+		tabHost = getTabHost();
+		getLayoutInflater().inflate(R.layout.main2,
+				tabHost.getTabContentView(), true);
 
 		Resources resources = getResources();
 		TabHost.TabSpec details = tabHost.newTabSpec("review").setContent(
@@ -79,67 +84,18 @@ public class ReviewActivity extends TabActivity implements ViewFactory,
 		tabHost.addTab(details);
 		tabHost.addTab(gallery);
 		tabHost.addTab(reservation);
+
+		tabHost.setCurrentTabByTag("photos");//calling this method will trigger onTabChanged() method
 		tabHost.setOnTabChangedListener(this);
+		init(tabHost.getCurrentTabTag());
 
-		TextView titleTextView = (TextView) findViewById(R.id.name);
-		titleTextView.setText("Jail\'s Highrise Curry Den");
-
-		comments = getResources().getStringArray(R.array.comments);
-		switcher = (TextSwitcher) findViewById(R.id.reviews);
-		switcher.setFactory(this);
-
-		// photo=(ImageView) findViewById(R.id.photo);
-		photo = (ImageSwitcher) findViewById(R.id.photo);
-		photo.setFactory(new ViewFactory() {
-
-			@Override
-			public View makeView() {
-				// TODO Auto-generated method stub
-				ImageView imageView = new ImageView(ReviewActivity.this);
-				imageView.setScaleType(ScaleType.CENTER_INSIDE);
-				return imageView;
-			}
-		});
-		Gallery photos = (Gallery) findViewById(R.id.gallery);
-		photos.setAdapter(new GalleryAdapter());
-		photos.setOnItemSelectedListener(this);
-
-		peopleLabel = (TextView) findViewById(R.id.people_label);
-		peopleLabelFormatString = peopleLabel.getText().toString();
-
-		date = (Button) findViewById(R.id.date);
-		dateFormat = new SimpleDateFormat(date.getText().toString());
-
-		time = (Button) findViewById(R.id.time);
-		timeFormat = new SimpleDateFormat(time.getText().toString());
-
-		Calendar calendar = Calendar.getInstance();
-		if (calendar.get(Calendar.HOUR_OF_DAY) >= 16) {
-			calendar.add(Calendar.DATE, 1);
-		}
-
-		calendar.set(Calendar.HOUR_OF_DAY, 18);
-		calendar.clear(Calendar.MINUTE);
-		calendar.clear(Calendar.SECOND);
-		calendar.clear(Calendar.MILLISECOND);
-
-		Date reservationDate = calendar.getTime();
-		date.setText(dateFormat.format(reservationDate));
-		time.setText(timeFormat.format(reservationDate));
-		date.setOnClickListener(this);
-		time.setOnClickListener(this);
-
-		SeekBar people = (SeekBar) findViewById(R.id.people);
-		people.setOnSeekBarChangeListener(this);
-		peopleLabel.setText(String.format(peopleLabelFormatString, people
-				.getProgress() + 1));
 	}
 
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		switcherCommentsHandler.postDelayed(this, 5 * 1000);
+		Log.v("kang", "kang-currentTab=" + tabHost.getCurrentTabTag());
 	}
 
 	@Override
@@ -239,7 +195,7 @@ public class ReviewActivity extends TabActivity implements ViewFactory,
 	public void onDateSet(DatePicker view, int year, int monthOfYear,
 			int dayOfMonth) {
 		// TODO Auto-generated method stub
-		Calendar calendar=Calendar.getInstance();
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, year);
 		calendar.set(Calendar.MONTH, monthOfYear);
 		calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -249,7 +205,7 @@ public class ReviewActivity extends TabActivity implements ViewFactory,
 	@Override
 	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 		// TODO Auto-generated method stub
-		Calendar calendar=Calendar.getInstance();
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
 		calendar.set(Calendar.MINUTE, minute);
 		time.setText(timeFormat.format(calendar.getTime()));
@@ -258,6 +214,72 @@ public class ReviewActivity extends TabActivity implements ViewFactory,
 	@Override
 	public void onTabChanged(String tabId) {
 		// TODO Auto-generated method stub
-		Log.v("kang","kang-tabId="+tabId);
+		Log.v("kang", "kang-tabId=" + tabId);
+		init(tabId);
+	}
+
+	public void init(String tabId) {
+		if (tabId.equals("reservation") && !reservationInit) {
+			Log.v("kang", "kang-tabId=" + tabId + ",reservationInit="
+					+ reservationInit);
+			reservationInit = true;
+			peopleLabel = (TextView) findViewById(R.id.people_label);
+			peopleLabelFormatString = peopleLabel.getText().toString();
+
+			date = (Button) findViewById(R.id.date);
+			dateFormat = new SimpleDateFormat(date.getText().toString());
+
+			time = (Button) findViewById(R.id.time);
+			timeFormat = new SimpleDateFormat(time.getText().toString());
+
+			Calendar calendar = Calendar.getInstance();
+			if (calendar.get(Calendar.HOUR_OF_DAY) >= 16) {
+				calendar.add(Calendar.DATE, 1);
+			}
+
+			calendar.set(Calendar.HOUR_OF_DAY, 18);
+			calendar.clear(Calendar.MINUTE);
+			calendar.clear(Calendar.SECOND);
+			calendar.clear(Calendar.MILLISECOND);
+
+			Date reservationDate = calendar.getTime();
+			date.setText(dateFormat.format(reservationDate));
+			time.setText(timeFormat.format(reservationDate));
+			date.setOnClickListener(this);
+			time.setOnClickListener(this);
+
+			SeekBar people = (SeekBar) findViewById(R.id.people);
+			people.setOnSeekBarChangeListener(this);
+			peopleLabel.setText(String.format(peopleLabelFormatString, people
+					.getProgress() + 1));
+		} else if (tabId.equals("photos") && !photoInit) {
+			Log.v("kang", "kang-tabId=" + tabId + ",photoInit=" + photoInit);
+			photoInit = true;
+			// photo=(ImageView) findViewById(R.id.photo);
+			photo = (ImageSwitcher) findViewById(R.id.photo);
+			photo.setFactory(new ViewFactory() {
+
+				@Override
+				public View makeView() {
+					// TODO Auto-generated method stub
+					ImageView imageView = new ImageView(ReviewActivity2.this);
+					imageView.setScaleType(ScaleType.CENTER_INSIDE);
+					return imageView;
+				}
+			});
+			Gallery photos = (Gallery) findViewById(R.id.gallery);
+			photos.setAdapter(new GalleryAdapter());
+			photos.setOnItemSelectedListener(this);
+		} else if (tabId.equals("review") && !reviewInit) {
+			Log.v("kang", "kang-tabId=" + tabId + ",reviewInit=" + reviewInit);
+			reviewInit=true;
+			TextView titleTextView = (TextView) findViewById(R.id.name);
+			titleTextView.setText("Jail\'s Highrise Curry Den");
+
+			comments = getResources().getStringArray(R.array.comments);
+			switcher = (TextSwitcher) findViewById(R.id.reviews);
+			switcher.setFactory(this);
+			switcherCommentsHandler.postDelayed(this, 5 * 1000);
+		}
 	}
 }
